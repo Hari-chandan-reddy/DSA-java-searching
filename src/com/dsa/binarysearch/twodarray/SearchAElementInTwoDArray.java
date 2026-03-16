888fddc92a853a58f2ec49a3da02c871f132649c
Pasula Hari Chandan Reddy <pharichandankkvs@gmail.com>
2026-03-16 18:17:19 +0530

    Add optimized O(n+m) search(staircase search) for element in row-wise and column-wise sorted 2D array.

--- src/com/dsa/binarysearch/twodarray/SearchAElementInTwoDArray.java
+++ src/com/dsa/binarysearch/twodarray/SearchAElementInTwoDArray.java
@@ -6,7 +6,7 @@ public class SearchAElementInTwoDArray {
     public static void main(String[] args) {
         int[][] arr = {{10, 20, 30, 40}, {15, 25, 35, 45}, {28, 29, 37, 49}, {33, 34, 38, 50}};
 
-        int target = 28;
+        int target = 20;
 
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[i].length; j++) {
